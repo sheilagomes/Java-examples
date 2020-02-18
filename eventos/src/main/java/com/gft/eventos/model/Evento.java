504977e3cb02ddcbd52bad3d0852a01830ca33c7
sheilagomes <sheilagomes1@gmail.com>
2020-02-18 13:57:43 -0300

    Adicionados os arquivos MVC

--- eventos/src/main/java/com/gft/eventos/model/Evento.java
+++ eventos/src/main/java/com/gft/eventos/model/Evento.java
@@ -1,16 +1,21 @@
 package com.gft.eventos.model;
 
+import org.hibernate.annotations.Entity;
+
+@Entity
 public class Evento {
-	private int id;
+	private long id;
 	private String nome;
 	private int capacidade;
+	private String data;
 	private float precoIngresso;
 	private String casa;
+	private String genero;
 	
-	public int getId() {
+	public long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	public String getNome() {
@@ -25,6 +30,12 @@ public class Evento {
 	public void setCapacidade(int capacidade) {
 		this.capacidade = capacidade;
 	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
 	public float getPrecoIngresso() {
 		return precoIngresso;
 	}
@@ -37,16 +48,27 @@ public class Evento {
 	public void setCasa(String casa) {
 		this.casa = casa;
 	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + capacidade;
 		result = prime * result + ((casa == null) ? 0 : casa.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		result = prime * result + Float.floatToIntBits(precoIngresso);
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -62,6 +84,18 @@ public class Evento {
 			if (other.casa != null)
 				return false;
 		} else if (!casa.equals(other.casa))
+			return false;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (genero == null) {
+			if (other.genero != null)
+				return false;
+		} else if (!genero.equals(other.genero))
+			return false;
+		if (id != other.id)
 			return false;
 		if (nome == null) {
 			if (other.nome != null)
